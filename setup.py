fc17be2e6bea71846f21181c71fb75749a501e64
Jelle Smet <development@smetj.net>
2012-10-05 22:30:21 +0200

    initial release

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+import sys
+
+PROJECT = 'testlap'
+
+# Change docs/sphinx/conf.py too!
+VERSION = '0.1'
+
+try:
+    long_description = open('README.rst', 'rt').read()
+except IOError:
+    long_description = ''
+
+
+setup(
+    name=PROJECT,
+    version=VERSION,
+
+    description='A naive framework to test and measure speed of Python code.',
+    long_description=long_description,
+
+    author='Jelle Smet',
+    author_email='development@smetj.net',
+
+    url='https://github.com/smetj/testlap',
+    download_url='https://github.com/smetj/molog/tarball/wishbone_based',
+
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: Apache Software License',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Intended Audience :: Programmers',
+                 ],
+
+    platforms=['Any'],
+    packages=['testlap'],
+    scripts=[],
+
+    provides=[],
+    install_requires=['prettytable'],
+
+    include_package_data=True,
+    )
